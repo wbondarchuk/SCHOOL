ce7a113ab130e8c3878fb108b11186e2c8f2ee95
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2021-10-18 16:37:43 +0700

    Add files via upload

---
+++ WEB/JS+HTML+CSS/Math/script1.js
@@ -0,0 +1,82 @@
+// console.log("Во поле березка стояла");
+// console.log("Раскудрявый клён зеленый, лист резной");
+// console.log("Иду в мокрых кроссах где бы я не был");
+
+// console.log("Co"+"do"+"lo"+"gi"+"a");
+
+// console.log("Айфон"<"Самсунг");
+// console.log(5>3);
+// console.log(10!=8);
+// console.log(8.1>8.11);
+
+// console.log(30-10*2);
+// console.log(25-5*2.7);
+
+// console.log("100"-1);
+// console.log("200"+100);
+
+// var proba=20;
+// console.log(proba);
+
+// var wow =13;
+// console.log (proba*wow);
+
+// console.log (wow*wow);
+
+// var yota=200;
+// yota=250-yota;
+// console.log(yota);
+
+// var apple=7;
+// var orange=10;
+// var basket= apple+orange;
+// console.log("Сумма фруктиков в корзине = ", basket);
+
+// var nevisokos = 365;
+// var visokos = 366;
+// var hours = 24;
+// var minutes = 60;
+// var seconds = 60;
+
+// console.log("В невисокосном году = ",365,"дней");
+// console.log("В високосном году = ",366,"дней");
+
+// console.log("В невисокосном году = ", nevisokos*hours ,"часов");
+// console.log("В високосном году = ", visokos*hours ,"часов");
+
+// console.log("В невисокосном году = ", nevisokos*hours*minutes, "минут");
+// console.log("В високосном году = ", visokos*hours*minutes, "минут");
+
+// console.log("В невисокосном году = ", nevisokos*hours*minutes*seconds, "секунд");
+// console.log("В високосном году = ", visokos*hours*minutes*seconds, "секунд");
+
+// alert("Проверочка")
+
+// prompt("Кока кола или пепси?")
+
+alert("Привет, ты попал на урок математики и русского одновременно! Жми ОК, если хочешь быстрее получить 5!") 
+alert("Молодец! Иди к доске! Назови свое имя") 
+
+var name = prompt("Напиши его здесь на доске") 
+
+var sravn = prompt("Ответь пожалуйста, какой будет результат операции 20 > 20.1") 
+
+
+if (sravn === "false") 
+alert("Ты молодец " + name + "!") 
+
+else if (sravn === "true") 
+var sravn = prompt("Подумай хорошо, какой будет результат операции 20 > 20.1") 
+
+alert("Итак, идем дальше") 
+
+
+var answ = prompt("Что получится после данной записи? '200'+'300'") 
+
+if (answ === "200300") 
+alert("Ты" + name + " Крутой!") 
+
+else if (answ === "500") 
+var answ = prompt("Подумай хорошо, что получится после данной записи? '200'+'300'") 
+
+alert("Ты получил 5!!!") 
